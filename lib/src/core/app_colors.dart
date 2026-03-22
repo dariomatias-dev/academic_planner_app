bad1a13814bababd78f871f58d98b1c942490727
dariomatias-dev <matiasdario75@gmail.com>
2026-03-22 12:19:19 -0300

    feat(lib/src): created color class
    - Developed a class to centralize the app's colors.

---
+++ lib/src/core/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF059669);
+  static const accent = Color(0xFF10B981);
+  static const bg = Color(0xFFF8FAFC);
+  static const textMain = Color(0xFF1E293B);
+  static const textSub = Color(0xFF64748B);
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const borderLight = Color(0xFFF1F5F9);
+  static const borderMedium = Color(0xFFE2E8F0);
+  static const dangerBg = Color(0xFFFEF2F2);
+  static const dangerText = Color(0xFFEF4444);
+  static const transparent = Colors.transparent;
+}
