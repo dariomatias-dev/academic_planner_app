deb3654518ff6b418b6f932fef02d06fbb584e87
dariomatias-dev <matiasdario75@gmail.com>
2026-03-23 17:32:09 -0300

    feat(lib/src/shared/widgets/dialogs/confirmation_dialog_widget.dart): added action button orientation
    - Implemented orientation for action buttons to be displayed in a row or column.

--- lib/src/shared/widgets/dialogs/confirmation_dialog_widget.dart
+++ lib/src/shared/widgets/dialogs/confirmation_dialog_widget.dart
@@ -4,57 +4,65 @@ import 'package:academic_planner/src/shared/widgets/buttons/button_widget.dart';
 import 'package:academic_planner/src/shared/widgets/dialogs/dialog_widget.dart';
 
 class ConfirmationDialogWidget extends StatelessWidget {
+  final IconData? icon;
   final String title;
   final String message;
+  final bool vertical;
   final String confirmLabel;
   final String cancelLabel;
   final VoidCallback onConfirm;
   final ButtonStyles confirmStyle;
-  final IconData? icon;
 
   const ConfirmationDialogWidget({
     super.key,
+    this.icon,
     required this.title,
     required this.message,
+    this.vertical = false,
     required this.onConfirm,
     this.confirmLabel = "Confirmar",
     this.cancelLabel = "Cancelar",
     this.confirmStyle = ButtonStyles.primary,
-    this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
+    final children = <Widget>[
+      Flexible(
+        fit: FlexFit.loose,
+        child: ButtonWidget(
+          onPressed: () => Navigator.pop(context),
+          label: cancelLabel,
+          style: ButtonStyles.neutral,
+          isFullWidth: true,
+        ),
+      ),
+      SizedBox(width: vertical ? 0 : 12, height: vertical ? 12 : 0),
+      Flexible(
+        fit: FlexFit.loose,
+        child: ButtonWidget(
+          onPressed: () {
+            Navigator.pop(context);
+            onConfirm();
+          },
+          label: confirmLabel,
+          style: confirmStyle,
+          isFullWidth: true,
+        ),
+      ),
+    ];
+
     return DialogWidget(
       title: title,
       message: message,
       icon: icon,
       iconColor: confirmStyle.backgroundColor,
-      actions: Row(
-        children: <Widget>[
-          Expanded(
-            child: ButtonWidget(
-              label: cancelLabel,
-              style: ButtonStyles.neutral,
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ),
-          const SizedBox(width: 12.0),
-          Expanded(
-            child: ButtonWidget(
-              label: confirmLabel,
-              style: confirmStyle,
-              onPressed: () {
-                Navigator.pop(context);
-
-                onConfirm();
-              },
-            ),
-          ),
-        ],
-      ),
+      actions: vertical
+          ? Column(
+              mainAxisSize: MainAxisSize.min,
+              children: children.reversed.toList(),
+            )
+          : Row(mainAxisSize: MainAxisSize.min, children: children),
     );
   }
 }
