fcb356562e994cb46b90111d028d228b4682ffb2
dariomatias-dev <matiasdario75@gmail.com>
2026-03-23 05:50:32 -0300

    refactor(lib/src/screens/activities/activities_screen.dart): implemented `AppBarWidget`
    - Simplified the screen through the implementation of `AppBarWidget`.

--- lib/src/screens/activities/activities_screen.dart
+++ lib/src/screens/activities/activities_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'package:academic_planner/src/core/app_colors.dart';
 import 'package:academic_planner/src/core/constants/mock_activities.dart';
 
 import 'package:academic_planner/src/shared/widgets/activity_card_widget.dart';
+import 'package:academic_planner/src/shared/widgets/app_bar_widget.dart';
 
 class ActivitiesScreenWidget extends StatelessWidget {
   const ActivitiesScreenWidget({super.key});
@@ -13,30 +13,13 @@ class ActivitiesScreenWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.bg,
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(24.0, 60.0, 24.0, 20.0),
-              child: Text(
-                "Tarefas",
-                style: GoogleFonts.plusJakartaSans(
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.w800,
-                  color: AppColors.textMain,
-                ),
-              ),
-            ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.fromLTRB(24.0, 0, 24.0, 120.0),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate((context, index) {
-                return ActivityCardWidget(activity: mockActivities[index]);
-              }, childCount: mockActivities.length),
-            ),
-          ),
-        ],
+      appBar: const AppBarWidget(title: "Tarefas"),
+      body: ListView.builder(
+        padding: const EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 120.0),
+        itemCount: mockActivities.length,
+        itemBuilder: (context, index) {
+          return ActivityCardWidget(activity: mockActivities[index]);
+        },
       ),
     );
   }
