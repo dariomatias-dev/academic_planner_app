91eb5760ac4babeffb765380ce005a689032078e
dariomatias-dev <matiasdario75@gmail.com>
2026-03-24 06:46:56 -0300

    feat(lib/src/core): created list extension
    - Developed a list extension to simplify common list operations.

---
+++ lib/src/core/extensions/list_extension.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+extension ListExtension<R> on List<R> {
+  List<T> builder<T>(T Function(R e, int index) toElement) {
+    return List.generate(length, (index) {
+      return toElement(elementAt(index), index);
+    });
+  }
+
+  List<Widget> separated(
+    Widget Function() separatorBuilder,
+    Widget Function(R e, int index) itemBuilder,
+  ) {
+    return List.generate(isNotEmpty ? length * 2 - 1 : 0, (index) {
+      if (index % 2 == 1) {
+        return separatorBuilder();
+      }
+
+      final currentIndex = index ~/ 2;
+
+      return itemBuilder(elementAt(currentIndex), currentIndex);
+    });
+  }
+
+  List<R> filter(bool Function(R e) test) {
+    final result = <R>[];
+
+    for (final item in this) {
+      if (test(item)) {
+        result.add(item);
+      }
+    }
+
+    return result;
+  }
+
+  List<R> reverse() {
+    final result = <R>[];
+
+    for (var i = length - 1; i >= 0; i--) {
+      result.add(this[i]);
+    }
+
+    return result;
+  }
+}
