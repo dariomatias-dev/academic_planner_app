c2e92a822a3ac0819cee67ad9f3a4f6df67c779f
dariomatias-dev <matiasdario75@gmail.com>
2026-03-24 22:45:01 -0300

    feat(lib/src/screens/create_task/widgets): componentized tag creation dialog
    - Developed a component for the tag creation dialog to improve separation of responsibilities.

---
+++ lib/src/screens/create_task/widgets/create_tag_dialog_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import 'package:academic_planner/src/core/app_colors.dart';
+
+import 'package:academic_planner/src/shared/widgets/buttons/button_widget.dart';
+import 'package:academic_planner/src/shared/widgets/dialogs/dialog_widget.dart';
+
+class CreateTagDialogWidget extends StatefulWidget {
+  final Function(String value) onTagAdded;
+
+  const CreateTagDialogWidget({super.key, required this.onTagAdded});
+
+  @override
+  State<CreateTagDialogWidget> createState() => _CreateTagDialogWidgetState();
+}
+
+class _CreateTagDialogWidgetState extends State<CreateTagDialogWidget> {
+  final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DialogWidget(
+      title: "Nova Tag",
+      message: "Crie uma etiqueta personalizada.",
+      icon: Icons.style_rounded,
+      actions: Column(
+        children: <Widget>[
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              hintText: "Nome da tag",
+              filled: true,
+              fillColor: AppColors.bg,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.0),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(height: 40.0),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ButtonWidget(
+                  onPressed: () => Navigator.pop(context),
+                  style: ButtonStyles.neutral,
+                  label: 'Cancelar',
+                  isFullWidth: true,
+                ),
+              ),
+              const SizedBox(width: 12.0),
+              Expanded(
+                child: ButtonWidget(
+                  onPressed: () {
+                    if (_controller.text.isNotEmpty) {
+                      widget.onTagAdded(_controller.text);
+
+                      Navigator.pop(context);
+                    }
+                  },
+                  label: "Salvar",
+                  isFullWidth: true,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
