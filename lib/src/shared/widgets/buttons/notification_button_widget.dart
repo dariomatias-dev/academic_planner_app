2c55d3e7ac1c16e4decc2f74a2774603f5723659
dariomatias-dev <matiasdario75@gmail.com>
2026-03-29 19:25:02 -0300

    feat(lib/src/shared/widgets/buttons): created notification button component
    - Component developed to standardize the notification button icon element.

---
+++ lib/src/shared/widgets/buttons/notification_button_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:academic_planner/src/shared/widgets/icon_buttons/icon_button_widget.dart';
+
+class NotificationButtonWidget extends StatelessWidget {
+  const NotificationButtonWidget({super.key});
+
+  final hasNotification = true;
+
+  void handlePressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Stack(
+      clipBehavior: Clip.none,
+      children: <Widget>[
+        IconButtonWidget(
+          icon: Icons.notifications_none_rounded,
+          onPressed: handlePressed,
+          style: IconButtonStyle.primary,
+        ),
+        if (hasNotification)
+          Positioned(
+            top: -2.0,
+            right: -2.0,
+            child: Container(
+              width: 10.0,
+              height: 10.0,
+              decoration: BoxDecoration(
+                color: colorScheme.error,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
