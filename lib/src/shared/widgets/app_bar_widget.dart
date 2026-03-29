8c5d7f26a6d67911a4d9c254002dce7410d89b8e
dariomatias-dev <matiasdario75@gmail.com>
2026-03-29 16:49:04 -0300

    feat(lib/src): redesigned `AppBarWidget` component
    - Redesigned the `AppBarWidget` component for a better user experience.

--- lib/src/shared/widgets/app_bar_widget.dart
+++ lib/src/shared/widgets/app_bar_widget.dart
@@ -2,45 +2,88 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:academic_planner/src/core/app_colors.dart';
+import 'package:academic_planner/src/core/extensions/list_extension.dart';
+
+import 'package:academic_planner/src/shared/widgets/icon_buttons/icon_buttons.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String? label;
+  final String? title;
+  final List<Widget>? actions;
+  final bool? showBackButton;
+
   const AppBarWidget({
     super.key,
-    required this.title,
+    this.label,
+    this.title,
     this.actions,
-    this.leading,
-    this.centerTitle = false,
+    this.showBackButton,
   });
-
-  final String title;
-  final List<Widget>? actions;
-  final Widget? leading;
-  final bool centerTitle;
 
   @override
   Size get preferredSize => const Size.fromHeight(80.0);
 
   @override
   Widget build(BuildContext context) {
+    final canGoBack = Navigator.canPop(context);
+    final displayBackButton = showBackButton ?? canGoBack;
+
     return AppBar(
+      automaticallyImplyLeading: false,
+      surfaceTintColor: AppColors.white,
       backgroundColor: AppColors.white,
-      elevation: 0,
-      scrolledUnderElevation: 0,
-      toolbarHeight: 80.0,
-      leading: leading,
-      actions: actions,
-      centerTitle: centerTitle,
-      shape: const Border(
-        bottom: BorderSide(color: AppColors.borderMedium, width: 1.0),
-      ),
-      title: Text(
-        title,
-        style: GoogleFonts.plusJakartaSans(
-          color: AppColors.textMain,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w800,
+      elevation: 0.0,
+      titleSpacing: 0.0,
+      toolbarHeight: preferredSize.height,
+      title: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            if (displayBackButton)
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: BackIconButtonWidget(),
+              ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  if (label != null)
+                    Text(
+                      label!.toUpperCase(),
+                      style: GoogleFonts.plusJakartaSans(
+                        color: AppColors.accent,
+                        fontSize: 10.0,
+                        fontWeight: FontWeight.w900,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  if (title != null)
+                    Text(
+                      title!,
+                      style: GoogleFonts.plusJakartaSans(
+                        color: AppColors.textMain,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            if (actions != null)
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: actions!.separated(
+                  () => const SizedBox(width: 8.0),
+                  (action, index) => action,
+                ),
+              ),
+          ],
         ),
       ),
+      centerTitle: false,
     );
   }
 }
