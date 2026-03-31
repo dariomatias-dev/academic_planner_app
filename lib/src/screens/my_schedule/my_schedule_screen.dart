cba7731efd0047f25ddc3bb8e5c6116240a61410
dariomatias-dev <matiasdario75@gmail.com>
2026-03-31 11:28:06 -0300

    feat(lib/src/screens/my_schedule/my_schedule_screen.dart): added display of user disciplines
    - `UserDisciplinesNotifier` implemented to display the schedule of disciplines selected by the user.

--- lib/src/screens/my_schedule/my_schedule_screen.dart
+++ lib/src/screens/my_schedule/my_schedule_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:academic_planner/src/core/constants/disciplines/ads_disciplines.dart';
 import 'package:academic_planner/src/core/constants/schedules.dart';
 import 'package:academic_planner/src/core/extensions/list_extension.dart';
 
+import 'package:academic_planner/src/notifiers/user_disciplines_notifier.dart';
+
 import 'package:academic_planner/src/shared/utils/image_export.dart';
 import 'package:academic_planner/src/shared/widgets/app_bar_widget.dart';
 import 'package:academic_planner/src/shared/widgets/icon_buttons/icon_button_widget.dart';
 import 'package:academic_planner/src/shared/widgets/schedule_table_view/schedule_table_view_widget.dart';
-
-const Set<int> studentEnrolledIds = <int>{51, 52, 53, 54, 55};
 
 class MyScheduleScreen extends StatefulWidget {
   const MyScheduleScreen({super.key});
@@ -32,13 +33,16 @@ class _MyScheduleScreenState extends State<MyScheduleScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final userDisciplinesNotifier = context.watch<UserDisciplinesNotifier>();
 
     final enrolledDisciplines = adsDisciplines.filter(
-      (discipline) => studentEnrolledIds.contains(discipline.id),
+      (discipline) =>
+          userDisciplinesNotifier.selectedIds.contains(discipline.id),
     );
 
     final enrolledEntries = schedules.filter(
-      (entry) => studentEnrolledIds.contains(entry.disciplineId),
+      (entry) =>
+          userDisciplinesNotifier.selectedIds.contains(entry.disciplineId),
     );
 
     return Scaffold(
