106f4d1984e8f13527b3fe5cf4e9986bdb2a397b
dariomatias-dev <matiasdario75@gmail.com>
2026-04-04 18:02:11 -0300

    feat(lib/src/core/extensions): added status extension
    - Created an extension to centralize the label and color of activity statuses.

---
+++ lib/src/core/extensions/activity_status_extension.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:academic_planner/src/shared/models/activity_model.dart';
+
+extension ActivityStatusExtension on ActivityStatus {
+  String get label {
+    return switch (this) {
+      ActivityStatus.draft => "Rascunho",
+      ActivityStatus.pending => "Pendente",
+      ActivityStatus.inProgress => "Em Andamento",
+      ActivityStatus.completed => "Concluído",
+      ActivityStatus.canceled => "Cancelado",
+    };
+  }
+
+  Color color(ColorScheme colorScheme) {
+    return switch (this) {
+      ActivityStatus.completed => Colors.teal,
+      ActivityStatus.inProgress => colorScheme.secondary,
+      ActivityStatus.canceled => colorScheme.error,
+      ActivityStatus.draft => colorScheme.onSurface.withAlpha(120),
+      ActivityStatus.pending => colorScheme.onSurface.withAlpha(100),
+    };
+  }
+}
