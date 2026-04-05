cb42276d32e7a500d406dca28aa602a56e0d27d4
dariomatias-dev <matiasdario75@gmail.com>
2026-04-05 14:55:05 -0300

    feat(lib/src/notifiers): added activity notifier
    - Created activity notifier to manage activity state.

---
+++ lib/src/notifiers/activity_notifier.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import 'package:academic_planner/src/core/result/result.dart';
+
+import 'package:academic_planner/src/data/repositories/activity/activity_repository.dart';
+
+import 'package:academic_planner/src/shared/models/activity_model.dart';
+
+class ActivityNotifier extends ChangeNotifier {
+  final ActivityRepository repository;
+
+  ActivityNotifier(this.repository);
+
+  final activities = <ActivityModel>[];
+  bool loading = false;
+
+  Future<Result<void>> addActivity(ActivityModel activity) async {
+    final result = await repository.addActivity(activity);
+
+    return await result.foldAsync(
+      onSuccess: (_) async {
+        await getActivities();
+
+        return Success(null);
+      },
+      onFailure: (failure) async {
+        return FailureResult(failure);
+      },
+    );
+  }
+
+  Future<Result<List<ActivityModel>>> getActivities() async {
+    loading = true;
+    notifyListeners();
+
+    final result = await repository.getActivities();
+
+    final output = await result.foldAsync<Result<List<ActivityModel>>>(
+      onSuccess: (list) async {
+        activities
+          ..clear()
+          ..addAll(list);
+
+        return Success(list);
+      },
+      onFailure: (failure) async {
+        return FailureResult(failure);
+      },
+    );
+
+    loading = false;
+    notifyListeners();
+
+    return output;
+  }
+
+  Future<Result<ActivityModel?>> getActivityById(String id) async {
+    return await repository.getActivityById(id);
+  }
+
+  Future<Result<void>> updateActivity(ActivityModel activity) async {
+    final result = await repository.updateActivity(activity);
+
+    return await result.foldAsync(
+      onSuccess: (_) async {
+        await getActivities();
+
+        return Success(null);
+      },
+      onFailure: (failure) async {
+        return FailureResult(failure);
+      },
+    );
+  }
+
+  Future<Result<void>> deleteActivity(String id) async {
+    final result = await repository.deleteActivity(id);
+
+    return await result.foldAsync(
+      onSuccess: (_) async {
+        await getActivities();
+
+        return Success(null);
+      },
+      onFailure: (failure) async {
+        return FailureResult(failure);
+      },
+    );
+  }
+}
