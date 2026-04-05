a69e36bbea4ddf9724d0d62a710446dc21c325f8
dariomatias-dev <matiasdario75@gmail.com>
2026-04-05 18:51:00 -0300

    refactor(lib/src/screens/activity_form): moved date picker field to separate file
    - Date picker field moved to its own file to improve separation of responsibilities.

--- lib/src/screens/activity_form/activity_form_screen.dart
+++ lib/src/screens/activity_form/activity_form_screen.dart
@@ -3,15 +3,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
 
-import 'package:academic_planner/src/core/app_colors.dart';
 import 'package:academic_planner/src/core/app_validators.dart';
 import 'package:academic_planner/src/core/constants/disciplines/ads_disciplines.dart';
 import 'package:academic_planner/src/core/constants/mock_activities.dart';
 import 'package:academic_planner/src/core/extensions/activity_status_extension.dart';
 import 'package:academic_planner/src/core/extensions/list_extension.dart';
 
+import 'package:academic_planner/src/screens/activity_form/widgets/activity_form_date_picker_widget.dart';
 import 'package:academic_planner/src/screens/activity_form/widgets/activity_form_description_field/activity_form_description_field_widget.dart';
 import 'package:academic_planner/src/screens/activity_form/widgets/activity_form_discipline_picker/activity_form_discipline_picker_widget.dart';
 import 'package:academic_planner/src/screens/activity_form/widgets/activity_form_section_title_widget.dart';
@@ -531,103 +530,6 @@ class ActivityFormTagSelectorWidget extends StatelessWidget {
           }),
         ),
       ],
-    );
-  }
-}
-
-class ActivityFormDatePickerWidget extends StatelessWidget {
-  final DateTime? dueDate;
-  final VoidCallback onTap;
-  final VoidCallback onClear;
-  final bool isRequired;
-
-  const ActivityFormDatePickerWidget({
-    super.key,
-    this.dueDate,
-    required this.onTap,
-    required this.onClear,
-    this.isRequired = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        decoration: BoxDecoration(
-          color: colorScheme.surface,
-          borderRadius: BorderRadius.circular(24.0),
-          border: Border.all(
-            color:
-                Theme.of(context).dividerTheme.color ?? AppColors.transparent,
-          ),
-        ),
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(12.0),
-              decoration: BoxDecoration(
-                color: colorScheme.primary.withAlpha(20),
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                Icons.calendar_today_rounded,
-                color: colorScheme.primary,
-                size: 24.0,
-              ),
-            ),
-            const SizedBox(width: 16.0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                ActivityFormLabelWidget(
-                  label: "Data de Entrega",
-                  isRequired: isRequired,
-                  fontSize: 11.0,
-                ),
-                Text(
-                  dueDate == null
-                      ? "Definir prazo"
-                      : DateFormat('dd / MM / yyyy').format(dueDate!),
-                  style: GoogleFonts.plusJakartaSans(
-                    color: colorScheme.onSurface,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-              ],
-            ),
-            const Spacer(),
-            if (dueDate != null) ...<Widget>[
-              GestureDetector(
-                onTap: (onClear),
-                child: Container(
-                  padding: const EdgeInsets.all(4.0),
-                  decoration: BoxDecoration(
-                    color: colorScheme.error.withAlpha(20),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.close_rounded,
-                    color: colorScheme.error,
-                    size: 22.0,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 12.0),
-            ],
-            const SizedBox(width: 4.0),
-            Icon(
-              Icons.edit_calendar_rounded,
-              color: colorScheme.primary,
-              size: 22.0,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
