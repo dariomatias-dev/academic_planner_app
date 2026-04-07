bd8f4871642ed10c7f50bcf712ebff19986f066d
dariomatias-dev <matiasdario75@gmail.com>
2026-04-07 08:42:16 -0300

    refactor(lib/src/shared/widgets): moved activity dialogs to separate folder
    - Created folder to centralize activity dialog components.

--- lib/src/shared/utils/handle_activity_deletion.dart
+++ lib/src/shared/utils/handle_activity_deletion.dart
@@ -6,9 +6,9 @@ import 'package:academic_planner/src/controllers/activity_controller.dart';
 import 'package:academic_planner/src/core/result/failure.dart';
 
 import 'package:academic_planner/src/shared/models/activity_model.dart';
-import 'package:academic_planner/src/shared/widgets/activity_card/activity_delete_dialog_widget.dart';
-import 'package:academic_planner/src/shared/widgets/activity_card/activity_removal_failure_dialog_widget.dart';
-import 'package:academic_planner/src/shared/widgets/activity_card/activity_removal_success_dialog_widget.dart';
+import 'package:academic_planner/src/shared/widgets/activity_dialogs/activity_delete_dialog_widget.dart';
+import 'package:academic_planner/src/shared/widgets/activity_dialogs/activity_removal_failure_dialog_widget.dart';
+import 'package:academic_planner/src/shared/widgets/activity_dialogs/activity_removal_success_dialog_widget.dart';
 
 Future<bool> handleActivityDeletion({
   required BuildContext context,
