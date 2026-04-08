d1dfb9ed12d087ecca7511de0697cb211f4648c7
dariomatias-dev <matiasdario75@gmail.com>
2026-04-07 22:30:48 -0300

    feat(lib/src/shared/widgets): added URL open error dialog
    - Created dialog to provide error feedback when failing to open a URL in the browser.

---
+++ lib/src/shared/widgets/link_opening_failure_dialog_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:academic_planner/src/shared/widgets/buttons/button/button_widget.dart';
+import 'package:academic_planner/src/shared/widgets/dialogs/dialog_widget.dart';
+
+class LinkOpeningFailureDialogWidget extends StatelessWidget {
+  const LinkOpeningFailureDialogWidget({super.key});
+
+  static Future<void> show(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return const LinkOpeningFailureDialogWidget();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return DialogWidget(
+      title: "Não foi possível abrir",
+      message:
+          "Ocorreu um problema ao tentar abrir o documento no seu navegador. Por favor, tente novamente em instantes.",
+      icon: Icons.link_off_rounded,
+      iconColor: colorScheme.error,
+      actions: ButtonWidget(
+        label: "Fechar",
+        onPressed: () => Navigator.pop(context),
+        style: AppButtonStyle.neutral,
+        isFullWidth: true,
+      ),
+    );
+  }
+}
