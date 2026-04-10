093405c9fe16ff944006c878f5a824e9241fc5f3
dariomatias-dev <matiasdario75@gmail.com>
2026-04-09 22:11:22 -0300

    feat(lib/src/data/filters/activity_filter.dart): added `copyWith`
    - Created `copyWith` method to simplify copying filter values.

--- lib/src/data/filters/activity_filter.dart
+++ lib/src/data/filters/activity_filter.dart
@@ -1,9 +1,11 @@
+import 'package:academic_planner/src/shared/models/activity_model.dart';
+
 class ActivityFilter {
   final String? search;
   final int? disciplineId;
   final DateTime? startDate;
   final DateTime? endDate;
-  final String? status;
+  final ActivityStatus? status;
 
   const ActivityFilter({
     this.search,
@@ -12,4 +14,20 @@ class ActivityFilter {
     this.endDate,
     this.status,
   });
+
+  ActivityFilter copyWith({
+    String? search,
+    int? disciplineId,
+    DateTime? startDate,
+    DateTime? endDate,
+    ActivityStatus? status,
+  }) {
+    return ActivityFilter(
+      search: search ?? this.search,
+      disciplineId: disciplineId ?? this.disciplineId,
+      startDate: startDate ?? this.startDate,
+      endDate: endDate ?? this.endDate,
+      status: status ?? this.status,
+    );
+  }
 }
