103a2309f20ad0a1b413ca613dc3e97ff4eb2801
dariomatias-dev <matiasdario75@gmail.com>
2026-04-11 14:03:32 -0300

    feat(lib/src/shared/widgets/inputs): created password field component
    - Developed a component to standardize the password field.

---
+++ lib/src/shared/widgets/inputs/password_input_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:academic_planner/src/shared/widgets/inputs/input_widget.dart';
+
+class PasswordInputWidget extends StatefulWidget {
+  final TextEditingController controller;
+  final String hint;
+  final String? Function(String? value)? validator;
+  final InputStyle style;
+
+  const PasswordInputWidget({
+    super.key,
+    required this.controller,
+    this.hint = "••••••••",
+    this.validator,
+    this.style = InputStyle.primary,
+  });
+
+  @override
+  State<PasswordInputWidget> createState() => _PasswordInputWidgetState();
+}
+
+class _PasswordInputWidgetState extends State<PasswordInputWidget> {
+  bool _isObscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return InputWidget(
+      controller: widget.controller,
+      hint: widget.hint,
+      validator: widget.validator,
+      style: widget.style,
+      obscureText: _isObscured,
+      prefixIcon: const Icon(Icons.lock_outline_rounded, size: 20.0),
+      suffix: IconButton(
+        onPressed: () {
+          setState(() => _isObscured = !_isObscured);
+        },
+        icon: Icon(
+          _isObscured
+              ? Icons.visibility_outlined
+              : Icons.visibility_off_outlined,
+          size: 20.0,
+        ),
+      ),
+    );
+  }
+}
