505c280ae8638f9e6068725dcd42982f1cd36b91
dariomatias-dev <matiasdario75@gmail.com>
2026-04-11 21:46:03 -0300

    feat(lib/src/shared/models): created login model
    - Developed a model to represent login data.

---
+++ lib/src/shared/models/auth/login_model.dart
@@ -0,0 +1,10 @@
+class LoginModel {
+  final String email;
+  final String password;
+
+  LoginModel({required this.email, required this.password});
+
+  Map<String, dynamic> toMap() {
+    return {'email': email, 'password': password};
+  }
+}
