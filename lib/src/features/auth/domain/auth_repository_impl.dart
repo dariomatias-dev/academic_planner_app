70edeef5d5f3c39d1dbf10b276c1a24cb49bcb4d
dariomatias-dev <matiasdario75@gmail.com>
2026-04-14 10:56:51 -0300

    feat(lib/src/features/auth): implemented authentication repository
    - Developed the authentication repository implementation consuming the authentication service.

---
+++ lib/src/features/auth/domain/auth_repository_impl.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:academic_planner/src/features/auth/data/auth_service.dart';
+import 'package:academic_planner/src/features/auth/domain/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  AuthRepositoryImpl(this._service);
+
+  final AuthService _service;
+
+  @override
+  User? get currentUser => _service.currentUser;
+
+  @override
+  Future<UserCredential> signIn(String email, String password) {
+    return _service.signIn(email, password);
+  }
+
+  @override
+  Future<UserCredential> signUp(String email, String password) {
+    return _service.signUp(email, password);
+  }
+
+  @override
+  Future<void> signOut() {
+    return _service.signOut();
+  }
+
+  @override
+  Stream<User?> authStateChanges() {
+    return _service.authStateChanges();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    return _service.sendEmailVerification();
+  }
+
+  @override
+  Future<void> reloadUser() {
+    return _service.reloadUser();
+  }
+}
