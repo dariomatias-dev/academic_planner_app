38f6c84f2d63ef8608cc0553a36c3d4f87d98c46
dariomatias-dev <matiasdario75@gmail.com>
2026-04-16 22:26:08 -0300

    feat(lib/src/features/user/data): create Firestore user service
    - Developed a service for managing users in Firestore.

---
+++ lib/src/features/user/data/services/user_firestore_service.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:academic_planner/src/features/user/data/models/user_model.dart';
+
+class UserFirestoreService {
+  UserFirestoreService(this._firestore);
+
+  final FirebaseFirestore _firestore;
+
+  CollectionReference get _usersRef => _firestore.collection('users');
+
+  Future<void> saveUser(UserModel user) async {
+    await _usersRef.doc(user.id).set(user.toMap());
+  }
+
+  Future<DocumentSnapshot> getUserDoc(String uid) async {
+    return await _usersRef.doc(uid).get();
+  }
+
+  Future<void> updateUser(UserModel user) async {
+    await _usersRef.doc(user.id).update(user.toMap());
+  }
+
+  Future<void> deleteUser(String uid) async {
+    await _usersRef.doc(uid).delete();
+  }
+}
