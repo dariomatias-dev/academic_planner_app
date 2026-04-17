05aee64be0db9d1e7c754af3cec4698ba739c1f8
dariomatias-dev <matiasdario75@gmail.com>
2026-04-17 20:12:23 -0300

    feat(lib/src/features/settings/widgets): create section component
    - Developed component for options section element.

---
+++ lib/src/features/settings/widgets/settings_section_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SettingsSectionWidget extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+
+  const SettingsSectionWidget({
+    super.key,
+    required this.title,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Column(
+      spacing: 16.0,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 4.0),
+          child: Text(
+            title.toUpperCase(),
+            style: GoogleFonts.plusJakartaSans(
+              color: colorScheme.onSurface.withAlpha(100),
+              fontSize: 11.0,
+              fontWeight: FontWeight.w900,
+              letterSpacing: 1.5,
+            ),
+          ),
+        ),
+        ...children,
+      ],
+    );
+  }
+}
