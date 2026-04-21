67a5b3d56dd58e33e5913af8c92e1d57592d05e6
dariomatias-dev <matiasdario75@gmail.com>
2026-04-21 18:13:00 -0300

    feat(lib/src/features/users/di/user_providers.dart): add user filter injection and provider
    - Added user filter injection.
    - Created a provider to simplify fetching users.

--- lib/src/features/users/di/user_providers.dart
+++ lib/src/features/users/di/user_providers.dart
@@ -6,7 +6,10 @@ import 'package:academic_planner/src/features/users/data/repositories/user_repos
 import 'package:academic_planner/src/features/users/data/services/user_firestore_service.dart';
 import 'package:academic_planner/src/features/users/domain/entities/user_entity.dart';
 import 'package:academic_planner/src/features/users/domain/repositories/user_repository.dart';
+import 'package:academic_planner/src/features/users/domain/value_objects/user_filter.dart';
+import 'package:academic_planner/src/features/users/presentation/providers/user_filter_notifier.dart';
 import 'package:academic_planner/src/features/users/presentation/state/user_notifier.dart';
+import 'package:academic_planner/src/features/users/presentation/state/user_view_model.dart';
 
 final userFirestoreServiceProvider = Provider<UserFirestoreService>((ref) {
   final firestore = ref.watch(firestoreProvider);
@@ -25,3 +28,15 @@ final userNotifierProvider = AsyncNotifierProvider<UserNotifier, UserEntity?>(
     return UserNotifier();
   },
 );
+
+final userFilterProvider = NotifierProvider<UserFilterNotifier, UserFilter>(
+  UserFilterNotifier.new,
+);
+
+final usersProvider = FutureProvider<List<UserEntity>>((ref) async {
+  final repository = ref.watch(userRepositoryProvider);
+  final filter = ref.watch(userFilterProvider);
+  final viewModel = UserViewModel(repository);
+
+  return await viewModel.listUsers(role: filter.role, query: filter.query);
+});
