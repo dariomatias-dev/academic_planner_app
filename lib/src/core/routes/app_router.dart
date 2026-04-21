898020775b4302f4940367a2edfbacf1cca4f70e
dariomatias-dev <matiasdario75@gmail.com>
2026-04-21 16:54:07 -0300

    feat(lib/src): reposition edit profile screen folder
    - Moved the edit profile screen to the users feature screens folder.

--- lib/src/core/routes/app_router.dart
+++ lib/src/core/routes/app_router.dart
@@ -12,7 +12,7 @@ import 'package:academic_planner/src/features/course_details/course_details_scre
 import 'package:academic_planner/src/features/disciplines/presentation/screens/discipline_details/discipline_details_screen.dart';
 import 'package:academic_planner/src/features/disciplines/presentation/screens/discipline_selection/discipline_selection_screen.dart';
 import 'package:academic_planner/src/features/disciplines/presentation/screens/disciplines/disciplines_screen.dart';
-import 'package:academic_planner/src/features/edit_profile/edit_profile_screen.dart';
+import 'package:academic_planner/src/features/users/presentation/screens/edit_profile/edit_profile_screen.dart';
 import 'package:academic_planner/src/features/forgot_password/forgot_password_screen.dart';
 import 'package:academic_planner/src/features/auth/presentation/screens/login/login_screen.dart';
 import 'package:academic_planner/src/features/my_schedule/my_schedule_screen.dart';
