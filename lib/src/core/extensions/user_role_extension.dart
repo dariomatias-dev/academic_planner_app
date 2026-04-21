9d1643f4f877273e24c853d2d68b4a63a2e4eb6f
dariomatias-dev <matiasdario75@gmail.com>
2026-04-21 14:19:17 -0300

    feat(lib/src/core/extensions): add user role extension
    - Created an extension to simplify retrieving color, icon, and label for user roles.

---
+++ lib/src/core/extensions/user_role_extension.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:academic_planner/src/features/users/domain/entities/user_entity.dart';
+
+extension UserRoleExtension on UserRole {
+  Color getColor(ColorScheme colorScheme) {
+    return switch (this) {
+      UserRole.admin => colorScheme.primary,
+      UserRole.teacher => Colors.teal,
+      UserRole.student => colorScheme.secondary,
+    };
+  }
+
+  String get label {
+    return switch (this) {
+      UserRole.admin => "administrador",
+      UserRole.teacher => "professor",
+      UserRole.student => "aluno",
+    };
+  }
+
+  IconData get icon {
+    return switch (this) {
+      UserRole.admin => Icons.admin_panel_settings_rounded,
+      UserRole.teacher => Icons.school_rounded,
+      UserRole.student => Icons.person_rounded,
+    };
+  }
+}
