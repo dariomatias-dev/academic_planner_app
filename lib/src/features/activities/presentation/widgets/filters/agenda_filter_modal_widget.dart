a9e485238e52b033ca8e8309c2fcbab7cf46a84c
dariomatias-dev <matiasdario75@gmail.com>
2026-04-23 05:36:59 -0300

    feat(lib/src/features/activities/presentation/widgets/filters/agenda_filter_modal_widget.dart): add new property
    - Added an optional filter parameter to define default values.

--- lib/src/features/activities/presentation/widgets/filters/agenda_filter_modal_widget.dart
+++ lib/src/features/activities/presentation/widgets/filters/agenda_filter_modal_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:academic_planner/src/features/activities/di/activity_providers.dart';
 import 'package:academic_planner/src/features/activities/domain/value_objects/activity_filter.dart';
 import 'package:academic_planner/src/features/activities/presentation/widgets/filters/filter_modal_layout_widget.dart';
 import 'package:academic_planner/src/features/activities/presentation/widgets/filters/sections/discipline_filter_section_widget.dart';
@@ -9,24 +8,31 @@ import 'package:academic_planner/src/features/activities/presentation/widgets/fi
 class AgendaFilterModalWidget extends ConsumerStatefulWidget {
   final VoidCallback onClear;
   final void Function(ActivityFilter filter) onApply;
+  final ActivityFilter? initialFilter;
 
   const AgendaFilterModalWidget({
     super.key,
     required this.onClear,
     required this.onApply,
+    this.initialFilter,
   });
 
   static Future<void> show(
     BuildContext context, {
     required VoidCallback onClear,
     required void Function(ActivityFilter filter) onApply,
+    ActivityFilter? initialFilter,
   }) {
     return showModalBottomSheet(
       context: context,
       isScrollControlled: true,
       backgroundColor: Colors.transparent,
       builder: (_) {
-        return AgendaFilterModalWidget(onClear: onClear, onApply: onApply);
+        return AgendaFilterModalWidget(
+          onClear: onClear,
+          onApply: onApply,
+          initialFilter: initialFilter,
+        );
       },
     );
   }
@@ -37,13 +43,7 @@ class AgendaFilterModalWidget extends ConsumerStatefulWidget {
 }
 
 class _AgendaFilterModalState extends ConsumerState<AgendaFilterModalWidget> {
-  late int? _selectedDisciplineId;
-
-  void _clearFilters() {
-    widget.onClear();
-
-    Navigator.pop(context);
-  }
+  late int? _selectedDisciplineId = widget.initialFilter?.disciplineId;
 
   void _applyFilters() {
     widget.onApply(ActivityFilter(disciplineId: _selectedDisciplineId));
@@ -52,18 +52,10 @@ class _AgendaFilterModalState extends ConsumerState<AgendaFilterModalWidget> {
   }
 
   @override
-  void initState() {
-    super.initState();
-
-    final currentFilter = ref.read(activityFilterNotifierProvider);
-    _selectedDisciplineId = currentFilter.disciplineId;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return FilterModalLayoutWidget(
       title: 'Filtros da Agenda',
-      onClear: _clearFilters,
+      onClear: widget.onClear,
       onApply: _applyFilters,
       children: <Widget>[
         DisciplineFilterSectionWidget(
