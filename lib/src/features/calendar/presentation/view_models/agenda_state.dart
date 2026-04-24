301e37d57d0b49ffec2f419bece8ed2987ee619c
dariomatias-dev <matiasdario75@gmail.com>
2026-04-23 21:09:40 -0300

    feat(lib/src/features/calendar/presentation): create agenda state
    - Developed an agenda state class.

---
+++ lib/src/features/calendar/presentation/view_models/agenda_state.dart
@@ -0,0 +1,32 @@
+import 'package:academic_planner/src/features/activities/domain/entities/activity.dart';
+import 'package:academic_planner/src/features/activities/domain/value_objects/activity_filter.dart';
+
+class AgendaState {
+  final List<Activity> activities;
+  final DateTime displayDate;
+  final DateTime selectedDate;
+  final ActivityFilter? filter;
+
+  AgendaState({
+    this.activities = const <Activity>[],
+    DateTime? displayDate,
+    DateTime? selectedDate,
+    this.filter,
+  }) : displayDate = displayDate ?? DateTime.now(),
+       selectedDate = selectedDate ?? DateTime.now();
+
+  AgendaState copyWith({
+    List<Activity>? activities,
+    DateTime? displayDate,
+    DateTime? selectedDate,
+    ActivityFilter? filter,
+    bool clearFilter = false,
+  }) {
+    return AgendaState(
+      activities: activities ?? this.activities,
+      displayDate: displayDate ?? this.displayDate,
+      selectedDate: selectedDate ?? this.selectedDate,
+      filter: clearFilter ? null : (filter ?? this.filter),
+    );
+  }
+}
