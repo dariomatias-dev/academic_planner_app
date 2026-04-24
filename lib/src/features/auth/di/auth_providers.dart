52504a8fe010d0aee0f5b6c26165934bad252ed1
dariomatias-dev <matiasdario75@gmail.com>
2026-04-24 10:42:50 -0300

    refactor(lib/src/features/auth): move auth notifier to providers folder
    - Moved the authentication notifier to the providers folder to improve separation of responsibilities.

--- lib/src/features/auth/di/auth_providers.dart
+++ lib/src/features/auth/di/auth_providers.dart
@@ -5,7 +5,7 @@ import 'package:academic_planner/src/core/di/firebase_providers.dart';
 import 'package:academic_planner/src/features/auth/data/services/auth_service.dart';
 import 'package:academic_planner/src/features/auth/domain/repositories/auth_repository.dart';
 import 'package:academic_planner/src/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:academic_planner/src/features/auth/presentation/state/auth_notifier.dart';
+import 'package:academic_planner/src/features/auth/presentation/providers/auth_notifier.dart';
 import 'package:academic_planner/src/features/users/domain/entities/user_entity.dart';
 
 final authServiceProvider = Provider<AuthService>((ref) {
