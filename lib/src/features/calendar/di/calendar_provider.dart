5574fd9b861ddc2007cff8b5ac12e9b08acdf947
dariomatias-dev <matiasdario75@gmail.com>
2026-04-23 21:12:40 -0300

    feat(lib/src/features/calendar): add agenda notifier injection
    - Implemented injection of the agenda notifier for use in the agenda screen.

---
+++ lib/src/features/calendar/di/calendar_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:academic_planner/src/features/calendar/presentation/providers/agenda_notifier.dart';
+import 'package:academic_planner/src/features/calendar/presentation/view_models/agenda_state.dart';
+
+final agendaNotifierProvider =
+    AsyncNotifierProvider<AgendaNotifier, AgendaState>(AgendaNotifier.new);
