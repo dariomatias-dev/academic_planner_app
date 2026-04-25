04eb66db9e9b0fb61c35385e8b88ec2a2105a1cc
dariomatias-dev <matiasdario75@gmail.com>
2026-04-24 22:40:43 -0300

    fix(lib/src/features/activities/presentation): corrected view model folder and file naming
    - Renamed view model folder and file to follow snake_case naming convention.

--- lib/src/features/activities/presentation/providers/activity_notifier.dart
+++ lib/src/features/activities/presentation/providers/activity_notifier.dart
@@ -6,7 +6,7 @@ import 'package:academic_planner/src/core/result/result.dart';
 import 'package:academic_planner/src/features/activities/di/activity_providers.dart';
 import 'package:academic_planner/src/features/activities/domain/entities/activity.dart';
 import 'package:academic_planner/src/features/activities/domain/value_objects/activity_filter.dart';
-import 'package:academic_planner/src/features/activities/presentation/viewmodels/activity_viewmodel.dart';
+import 'package:academic_planner/src/features/activities/presentation/view_models/activity_view_model.dart';
 
 class ActivityNotifier extends AsyncNotifier<void> {
   late final ActivityViewModel _viewModel;
