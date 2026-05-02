ebed257edf18f4149637a196ab51f81db1a017e2
dariomatias-dev <matiasdario75@gmail.com>
2026-05-02 10:42:11 -0300

    refactor(lib/src): relocate `NavigationNotifier`
    - Moved navigation notifier to the `core` notifiers folder.

--- lib/src/core/di/navigation_provider.dart
+++ lib/src/core/di/navigation_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:academic_planner/src/notifiers/navigation_notifier.dart';
+import 'package:academic_planner/src/core/notifiers/navigation_notifier.dart';
 
 final navigationNotifierProvider = NotifierProvider<NavigationNotifier, int>(
   NavigationNotifier.new,
