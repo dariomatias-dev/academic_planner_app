fc6ccffc4d72bc46a9da85058abcf5154152dc14
dariomatias-dev <matiasdario75@gmail.com>
2026-05-05 10:05:06 -0300

    feat(lib/src/features/categories/di/category_providers.dart): add `CategoriesNotifier` injection
    - Added injection for the categories notifier.

--- lib/src/features/categories/di/category_providers.dart
+++ lib/src/features/categories/di/category_providers.dart
@@ -2,11 +2,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:academic_planner/src/core/di/shared_preferences_provider.dart';
 
+import 'package:academic_planner/src/features/categories/data/models/category_model.dart';
 import 'package:academic_planner/src/features/categories/data/repositories/category_repository_impl.dart';
 import 'package:academic_planner/src/features/categories/domain/repositories/category_repository.dart';
+import 'package:academic_planner/src/features/categories/presentation/providers/categories_notifier.dart';
 
 final categoryRepositoryProvider = Provider<CategoryRepository>((ref) {
   final prefs = ref.read(sharedPreferencesServiceProvider);
 
   return CategoryRepositoryImpl(prefs);
 });
+
+final categoriesProvider =
+    NotifierProvider<CategoriesNotifier, List<CategoryModel>>(() {
+      return CategoriesNotifier();
+    });
