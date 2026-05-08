0b07af878c683f6f0e19cc41bd1fdf5483b70948
dariomatias-dev <matiasdario75@gmail.com>
2026-05-07 22:31:17 -0300

    feat(lib/src/features/tags/di/tag_providers.dart): added `TagNotifier` injection
    - Implemented the tag notifier injection.

--- lib/src/features/tags/di/tag_providers.dart
+++ lib/src/features/tags/di/tag_providers.dart
@@ -2,11 +2,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:academic_planner/src/core/di/shared_preferences_provider.dart';
 
+import 'package:academic_planner/src/features/tags/data/models/tag_model.dart';
 import 'package:academic_planner/src/features/tags/data/repositories/tag_repository_impl.dart';
 import 'package:academic_planner/src/features/tags/domain/repositories/tag_repository.dart';
+import 'package:academic_planner/src/features/tags/presentation/providers/tag_notifier.dart';
 
 final tagRepositoryProvider = Provider<TagRepository>((ref) {
   final prefs = ref.read(sharedPreferencesServiceProvider);
 
   return TagRepositoryImpl(prefs);
+});
+
+final tagNotifierProvider = NotifierProvider<TagNotifier, List<TagModel>>(() {
+  return TagNotifier();
 });
