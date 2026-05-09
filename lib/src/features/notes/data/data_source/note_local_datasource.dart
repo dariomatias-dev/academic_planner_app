e0d63f536420ca096cd54a8b016e8d22e434a78b
dariomatias-dev <matiasdario75@gmail.com>
2026-05-09 16:36:40 -0300

    feat(lib/src/features/notes/data): created note data source
    - Developed a local data source for managing notes.

---
+++ lib/src/features/notes/data/data_source/note_local_datasource.dart
@@ -0,0 +1,41 @@
+import 'package:sqflite/sqflite.dart';
+
+import 'package:academic_planner/src/core/database/tables/note_table.dart';
+
+class NoteLocalDataSource {
+  final Database db;
+
+  NoteLocalDataSource(this.db);
+
+  Future<void> insert(Map<String, dynamic> data) async {
+    await db.insert(NoteTable.tableName, data);
+  }
+
+  Future<List<Map<String, dynamic>>> getAll() async {
+    return await db.query(NoteTable.tableName, orderBy: 'updatedAt DESC');
+  }
+
+  Future<Map<String, dynamic>?> getById(String id) async {
+    final result = await db.query(
+      NoteTable.tableName,
+      where: 'id = ?',
+      whereArgs: [id],
+      limit: 1,
+    );
+
+    return result.isEmpty ? null : result.first;
+  }
+
+  Future<void> update(String id, Map<String, dynamic> data) async {
+    await db.update(
+      NoteTable.tableName,
+      data,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<void> delete(String id) async {
+    await db.delete(NoteTable.tableName, where: 'id = ?', whereArgs: [id]);
+  }
+}
