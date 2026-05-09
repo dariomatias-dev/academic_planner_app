7e9b5b0aa89598574d04b8a48e094c4b07a2e0bc
dariomatias-dev <matiasdario75@gmail.com>
2026-05-09 16:49:50 -0300

    feat(lib/src/features/notes/di/note_providers.dart): added `NoteNotifier` injection
    - Implemented the note notifier injection.

--- lib/src/features/notes/di/note_providers.dart
+++ lib/src/features/notes/di/note_providers.dart
@@ -5,6 +5,7 @@ import 'package:academic_planner/src/core/di/database_provider.dart';
 import 'package:academic_planner/src/features/notes/data/data_source/note_local_datasource.dart';
 import 'package:academic_planner/src/features/notes/data/repositories/note_repository_impl.dart';
 import 'package:academic_planner/src/features/notes/domain/repositories/note_repository.dart';
+import 'package:academic_planner/src/features/notes/presentation/providers/note_notifier.dart';
 
 final noteDatasourceProvider = Provider<NoteLocalDataSource>((ref) {
   final db = ref.watch(appDatabaseProvider);
@@ -17,3 +18,7 @@ final noteRepositoryProvider = Provider<NoteRepository>((ref) {
 
   return NoteRepositoryImpl(datasource);
 });
+
+final noteNotifierProvider = AsyncNotifierProvider<NoteNotifier, void>(
+  NoteNotifier.new,
+);
