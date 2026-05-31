23fdc6850a5ec190df22e9d7284733133f62146b
dariomatias-dev <matiasdario75@gmail.com>
2026-05-31 16:17:33 -0300

    feat(lib/src/features/categories/data/models/category_model.dart): added entity conversion methods
    - Added methods to convert entities to models and models to entities.

--- lib/src/features/categories/data/models/category_model.dart
+++ lib/src/features/categories/data/models/category_model.dart
@@ -1,7 +1,15 @@
+import 'package:academic_planner/src/features/categories/domain/entities/category.dart';
+
 class CategoryModel {
   final String name;
 
   const CategoryModel(this.name);
+
+  Category toEntity() => Category(name: name);
+
+  factory CategoryModel.fromEntity(Category category) {
+    return CategoryModel(category.name);
+  }
 
   Map<String, dynamic> toMap() {
     return {'name': name};
