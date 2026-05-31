2e9c04cb553bfde3b0d9b19f7ebf84b7fd831e19
dariomatias-dev <matiasdario75@gmail.com>
2026-05-31 16:23:31 -0300

    feat(lib/src/features/tags/data/models/tag_model.dart): added entity conversion methods
    - Added methods to convert entities to models and models to entities.

--- lib/src/features/tags/data/models/tag_model.dart
+++ lib/src/features/tags/data/models/tag_model.dart
@@ -1,7 +1,13 @@
+import 'package:academic_planner/src/features/tags/domain/entities/tag.dart';
+
 class TagModel {
   final String name;
 
   const TagModel(this.name);
+
+  Tag toEntity() => Tag(name: name);
+
+  factory TagModel.fromEntity(Tag tag) => TagModel(tag.name);
 
   Map<String, dynamic> toMap() {
     return {'name': name};
