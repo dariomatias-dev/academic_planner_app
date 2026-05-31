cf400b7275eabda0f25fef233f4137ae7b317a16
dariomatias-dev <matiasdario75@gmail.com>
2026-05-31 16:37:08 -0300

    feat(lib/src/features): made category and tag notifier view model properties private
    - Changed the view model properties of category and tag notifiers to private to prevent unnecessary exposure.

--- lib/src/features/categories/presentation/providers/categories_notifier.dart
+++ lib/src/features/categories/presentation/providers/categories_notifier.dart
@@ -7,15 +7,15 @@ import 'package:academic_planner/src/features/categories/domain/entities/categor
 import 'package:academic_planner/src/features/categories/presentation/view_models/categories_view_model.dart';
 
 class CategoriesNotifier extends AsyncNotifier<List<Category>> {
-  late final CategoriesViewModel viewModel;
+  late final CategoriesViewModel _viewModel;
 
   @override
   Future<List<Category>> build() async {
     final repo = ref.read(categoryRepositoryProvider);
 
-    viewModel = CategoriesViewModel(repo);
+    _viewModel = CategoriesViewModel(repo);
 
-    final result = await viewModel.load();
+    final result = await _viewModel.load();
 
     return result.fold(
       onSuccess: (data) => data,
@@ -25,7 +25,7 @@ class CategoriesNotifier extends AsyncNotifier<List<Category>> {
 
   Future<Result<List<Category>>> add(String name) async {
     final current = state.asData?.value ?? [];
-    final result = await viewModel.add(current, name);
+    final result = await _viewModel.add(current, name);
 
     result.when(onSuccess: (data) => state = AsyncData(data));
 
@@ -34,7 +34,7 @@ class CategoriesNotifier extends AsyncNotifier<List<Category>> {
 
   Future<Result<List<Category>>> edit(int index, String name) async {
     final current = state.asData?.value ?? [];
-    final result = await viewModel.update(current, index, name);
+    final result = await _viewModel.update(current, index, name);
 
     result.when(onSuccess: (data) => state = AsyncData(data));
 
@@ -43,7 +43,7 @@ class CategoriesNotifier extends AsyncNotifier<List<Category>> {
 
   Future<Result<List<Category>>> remove(int index) async {
     final current = state.asData?.value ?? [];
-    final result = await viewModel.remove(current, index);
+    final result = await _viewModel.remove(current, index);
 
     result.when(onSuccess: (data) => state = AsyncData(data));
 
--- lib/src/features/tags/presentation/providers/tag_notifier.dart
+++ lib/src/features/tags/presentation/providers/tag_notifier.dart
@@ -7,15 +7,15 @@ import 'package:academic_planner/src/features/tags/domain/entities/tag.dart';
 import 'package:academic_planner/src/features/tags/presentation/view_models/tag_view_model.dart';
 
 class TagNotifier extends AsyncNotifier<List<Tag>> {
-  late final TagViewModel viewModel;
+  late final TagViewModel _viewModel;
 
   @override
   Future<List<Tag>> build() async {
     final repo = ref.read(tagRepositoryProvider);
 
-    viewModel = TagViewModel(repo);
+    _viewModel = TagViewModel(repo);
 
-    final result = await viewModel.load();
+    final result = await _viewModel.load();
 
     return result.fold(
       onSuccess: (data) => data,
@@ -25,7 +25,7 @@ class TagNotifier extends AsyncNotifier<List<Tag>> {
 
   Future<Result<List<Tag>>> add(String name) async {
     final current = state.asData?.value ?? [];
-    final result = await viewModel.add(current, name);
+    final result = await _viewModel.add(current, name);
 
     result.when(onSuccess: (data) => state = AsyncData(data));
 
@@ -34,7 +34,7 @@ class TagNotifier extends AsyncNotifier<List<Tag>> {
 
   Future<Result<List<Tag>>> edit(int index, String name) async {
     final current = state.asData?.value ?? [];
-    final result = await viewModel.update(current, index, name);
+    final result = await _viewModel.update(current, index, name);
 
     result.when(onSuccess: (data) => state = AsyncData(data));
 
@@ -43,7 +43,7 @@ class TagNotifier extends AsyncNotifier<List<Tag>> {
 
   Future<Result<List<Tag>>> remove(int index) async {
     final current = state.asData?.value ?? [];
-    final result = await viewModel.remove(current, index);
+    final result = await _viewModel.remove(current, index);
 
     result.when(onSuccess: (data) => state = AsyncData(data));
 
