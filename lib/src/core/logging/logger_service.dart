b3feb8dad16496917eb3b2be2179b37b66a98c9d
dariomatias-dev <matiasdario75@gmail.com>
2026-06-06 15:58:00 -0300

    refactor(lib/src/core/logging): remove unused resources
    - Removed unused logging resources that were no longer necessary.

--- lib/src/core/logging/logger_service.dart
+++
@@ -1,9 +0,0 @@
-abstract class LoggerService {
-  void error(String message, {Object? error, StackTrace? stackTrace});
-
-  void warning(String message, {Object? error, StackTrace? stackTrace});
-
-  void info(String message);
-
-  void debug(String message);
-}
